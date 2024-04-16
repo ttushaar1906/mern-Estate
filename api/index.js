6989806b923b8f3c103def53f0f41c5de1576e84
Tushar Tharwani <ttushaar45@gmail.com>
2024-04-17 02:36:19 +0530

    database Connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+// mongoose.connect(process.env.MONGO)
+mongoose.connect("mongodb+srv://ttushaar45:tushar1906@cluster0.dtjlqer.mongodb.net/estate_mern?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+    console.log("Connected to database")
+})
+.catch((err)=>{
+    console.log("Error while connecting",err)
+})
 const app = express();
 
 app.listen(3000, ()=>{
