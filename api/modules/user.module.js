6f7ec16fee6c1078ff0bdbb249983df8df73f0f5
Tushar Tharwani <ttushaar45@gmail.com>
2024-04-28 14:28:20 +0530

    create user model

---
+++ api/modules/user.module.js
@@ -0,0 +1,28 @@
+import exp from "constants";
+import mongoose from "mongoose";
+
+// we are creating schemas i.e rules for user modules
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:true,
+        require:true,
+    },
+    email:{
+        type:String,
+        unique:true,
+        require:true,
+    },
+    password:{
+        type:String,
+        
+        require:true,
+    }
+},{ timestamps : true});
+
+//timestamps is used to keep record of account created and account updated
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
