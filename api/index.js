06f9bfb01321484bf25a5585f9b5d618cb23a318
Tushar Tharwani <ttushaar45@gmail.com>
2024-04-28 15:11:41 +0530

    creata api route and test

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
 dotenv.config();
 
 // mongoose.connect(process.env.MONGO)
@@ -16,3 +18,5 @@ const app = express();
 app.listen(3000, ()=>{
     console.log('Server is running on port 4545')
 })
+
+app.use('/api/user', userRouter);
