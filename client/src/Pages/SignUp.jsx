761e0bf015dcd1971d1dc69c87f9074adf22f947
Tushar Tharwani <ttushaar45@gmail.com>
2024-04-30 18:59:07 +0530

    connecting with db

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,27 +1,51 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value,
+    });
+  }
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    const res = await fetch('/api/auth/signUp', {
+      method:"POST",
+      headers:{
+        'Content-type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data)
+  }
+  console.log(formData)
   return (
     <div className="max-w-lg m-auto p-4 shadow-lg bg-grey mt-4">
       <h1 className="text-center py-8 font-bold text-3xl">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Enter Username"
           className="p-2 border rounded-md"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Enter Email"
           className="p-2 border rounded-md"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Enter Password"
           className="p-2 border rounded-md"
           id="password"
+          onChange={handleChange}
         />
         <button className="p-2 border uppercase rounded-md font-bold bg-third-color text-primary-color hover:bg-secondary-color hover:text-white">
           Sign Up
