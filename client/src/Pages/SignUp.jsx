6bc1eadfbc0ee970b2c9bb5427f268e087f1b934
Tushar Tharwani <ttushaar45@gmail.com>
2024-05-03 02:36:32 +0530

    signUp page design

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import { Link,useNavigate  } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 export default function SignUp() {
@@ -18,10 +18,10 @@ export default function SignUp() {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('/api/auth/signup', {
-        method: 'POST',
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
@@ -34,53 +34,56 @@ export default function SignUp() {
       }
       setLoading(false);
       setError(null);
-      navigate('/sign-in');
+      navigate("/sign-in");
     } catch (error) {
       setLoading(false);
       setError(error.message);
-    }
+    } 
   };
   return (
-    <div className="test">
-    <div className="max-w-lg m-auto p-4 shadow-lg bg-gray-200">
-      <h1 className="text-center py-8 font-bold text-3xl">Sign Up</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input
-          type="text"
-          placeholder="Enter Username"
-          className="p-2 border rounded-md"
-          id="username"
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          placeholder="Enter Email"
-          className="p-2 border rounded-md"
-          id="email"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Enter Password"
-          className="p-2 border rounded-md"
-          id="password"
-          onChange={handleChange}
-        />
-        <button
-          disabled={loading}
-          className="p-2 border uppercase rounded-md font-bold bg-third-color text-primary-color hover:bg-secondary-color hover:text-white"
-        >
-          {loading ? "Loading...." : "Sign Up"}
-        </button>
-        <div className="flex gap-2">
-          <p>Already have an account?</p>
-          <Link to="/sign-in">
-            <span className="text-primary-color font-bold">Sign In</span>
-          </Link>
-        </div>
-        {error && <p className="text-red mt-4">{error}</p>}
-      </form>
-    </div>
+    <div className="container flex-col mt-0 sm:flex-row sm:mt-24 shadow-lg">
+      <div className="w-1/2 order-2 sm:order-1 shadow-sm">
+        <img src="../src/assets/loginImg.jpeg" alt="" className="w-full object-cover bg-no-repeat block m-auto" />
+      </div>
+      <div className="max-w-lg p-2 bg-grey w-1/2 sm:order-2 shadow-md ">
+        <h1 className="text-center py-6 font-bold text-3xl">Sign Up</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full">
+          <input
+            type="text"
+            placeholder="Enter Username"
+            className="p-2 border rounded-md"
+            id="username"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="p-2 border rounded-md"
+            id="email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="p-2 border rounded-md"
+            id="password"
+            onChange={handleChange}
+          />
+          <button
+            disabled={loading}
+            className="p-2 border uppercase rounded-md font-bold bg-third-color text-primary-color hover:bg-secondary-color hover:text-white"
+          >
+            {loading ? "Loading...." : "Sign Up"}
+          </button>
+          <div className="flex gap-2">
+            <p>Already have an account?</p>
+            <Link to="/sign-in">
+              <span className="text-primary-color font-bold">Sign In</span>
+            </Link>
+          </div>
+          {error && <p className="text-red mt-4">{error}</p>}
+        </form>
+      </div>
     </div>
   );
 }
