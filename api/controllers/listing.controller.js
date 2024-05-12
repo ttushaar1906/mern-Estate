f429ff1b1aa89d07caa6affbe3f3dfebd461a951
Tushar Tharwani <ttushaar45@gmail.com>
2024-05-13 01:58:38 +0530

    list update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -26,3 +26,23 @@ export const createListing = async (req, res, next) => {
     }
 
   }
+
+  export const updateListing = async (req, res, next)=> {
+    const listing = await Listing.findById(req.params.id);
+    if(!listing){
+      return next(errorHandler(404,"Listing not found! "));
+    }
+    if(req.user.id !== listing.userRef){
+      return next(errorHandler(401, "You can only update your own listing"))
+    }
+    try {
+      const updatedListing = await Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+      );
+      res.status(200).json(updatedListing)
+    } catch (error) {
+      next(error)
+    }
+  }
